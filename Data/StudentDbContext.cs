5108db74be719414c8c219653648a2774a679fea
beyza-yesilyurtt <108866247+beyza-yesilyurtt@users.noreply.github.com>
2023-07-02 17:52:31 +0300

    first commit

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Student_IMS.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
